2fb1f05cd9945894919f717b39cfa9675fcd2bdd
Jiwook00 <jwkim775@gmail.com>
2020-11-18 22:05:59 +0900

    Feature AdminPage API
    1. 모든 맥주 리스트 가저오기
    2. 모든 유저 리스트 가저오기
    3. 모든 레포트 리스트 가저오기
    4. 정렬 기능 추가

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -9,38 +9,48 @@ const router = express.Router();
 //----------------------------------------------Beer
 router.get('/beerlist', async (req, res) => {
   try {
-    console.log(req.url);
+    console.log('::::::get /beerlist::::::::', req.url);
     const { url } = req;
-    // let limit = url.match(/limit=(\d+(\d)*)/);
-    // let page = url.match(/page=(\d+(\d)*)/);
-    // const start = limit * page - limit + 1;
-    // const end = limit * page;
-    const limit = Number(url.slice(16)[0] + url.slice(16)[1]);
-    const page = Number(url.slice(24)[0]);
-    const start = limit * page - limit + 1;
-    const end = limit * page;
-    const allBeerList = await Beer.findAll({
-      raw: true,
-    });
+    const limitArr: RegExpMatchArray | null = url.match(/limit=(\d+(\d)*)/);
+    const pageArr: RegExpMatchArray | null = url.match(/page=(\d+(\d)*)/);
+    const targetArr: RegExpMatchArray | null = url.match(/orderBy=(\w+(\w)*)/);
+    const sortArr: RegExpMatchArray | null = url.match(/orderDir=(\w+(\w)*)/);
+    if (targetArr && sortArr && limitArr && pageArr) {
+      const limit = Number(limitArr[1]);
+      const page = Number(pageArr[1]);
+      const start = limit * page - limit + 1;
+      const end = limit * page;
+      const target = String(targetArr[1]);
+      const sort = String(sortArr[1]);
 
-    const sendBeerList = await Beer.findAll({
-      raw: true,
-      where: {
-        id: {
-          [Sequelize.Op.between]: [start, end],
+      // const limit = Number(url.slice(16)[0] + url.slice(16)[1]);
+      // const page = Number(url.slice(24)[0]);
+      // const start = limit * page - limit + 1;
+      // const end = limit * page;
+      const allBeerList = await Beer.findAll({
+        raw: true,
+      });
+
+      const sendBeerList = await Beer.findAll({
+        raw: true,
+        where: {
+          id: {
+            [Sequelize.Op.between]: [start, end],
+          },
         },
-      },
-    });
+        order: [[target, sort]],
+      });
 
-    // const delay = setTimeout(function(){
-    //     res.status(200).json(sendAllBeerList)
-    // }, 5000)
+      // const delay = setTimeout(function(){
+      //     res.status(200).json(sendAllBeerList)
+      // }, 5000)
 
-    const total = String(allBeerList.length);
-    if (allBeerList) {
-      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
-      res.set('X-Total-Count', total);
-      return res.status(200).json(sendBeerList);
+      const total = String(allBeerList.length);
+      if (allBeerList) {
+        res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+        res.set('X-Total-Count', total);
+        return res.status(200).json(sendBeerList);
+      }
     }
   } catch (e) {
     return res.sendStatus(500);
@@ -146,35 +156,42 @@ router.get('/beerlist/:id', async (req, res) => {
 router.get('/userlist', async (req, res) => {
   try {
     const { url } = req;
-    console.log(':::::url::::::', url);
+    console.log(':::::get  /userlist::::::', url);
 
-    const limit = Number(url.slice(16)[0] + url.slice(16)[1]);
-    const page = Number(url.slice(24)[0]);
-    const start = limit * page - limit + 1;
-    const end = limit * page;
-    console.log(':::::limit:::::::', limit);
-    console.log(':::::page:::::::', page);
-    const allUserList = await User.findAll({
-      raw: true,
-      // where: {
-      //   id: {
-      //     [Sequelize.Op.between]: [start, end],
-      //   },
-      // },
-    });
-    const total = String(allUserList.length);
-    const sendUserList = await User.findAll({
-      raw: true,
-      where: {
-        id: {
-          [Sequelize.Op.between]: [start, end],
+    // const limit = Number(url.slice(16)[0] + url.slice(16)[1]);
+    // const page = Number(url.slice(24)[0]);
+    // const start = limit * page - limit + 1;
+    // const end = limit * page;
+    const limitArr: RegExpMatchArray | null = url.match(/limit=(\d+(\d)*)/);
+    const pageArr: RegExpMatchArray | null = url.match(/page=(\d+(\d)*)/);
+    const targetArr: RegExpMatchArray | null = url.match(/orderBy=(\w+(\w)*)/);
+    const sortArr: RegExpMatchArray | null = url.match(/orderDir=(\w+(\w)*)/);
+    if (targetArr && sortArr && limitArr && pageArr) {
+      const limit = Number(limitArr[1]);
+      const page = Number(pageArr[1]);
+      const start = limit * page - limit + 1;
+      const end = limit * page;
+      const target = String(targetArr[1]);
+      const sort = String(sortArr[1]);
+
+      const allUserList = await User.findAll({
+        raw: true,
+      });
+      const total = String(allUserList.length);
+      const sendUserList = await User.findAll({
+        raw: true,
+        where: {
+          id: {
+            [Sequelize.Op.between]: [start, end],
+          },
         },
-      },
-    });
-    if (allUserList) {
-      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
-      res.set('X-Total-Count', total);
-      return res.status(200).json(sendUserList);
+        order: [[target, sort]],
+      });
+      if (allUserList) {
+        res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+        res.set('X-Total-Count', total);
+        return res.status(200).json(sendUserList);
+      }
     }
   } catch (e) {
     return res.sendStatus(500);
@@ -246,30 +263,37 @@ router.delete('/userlist/:id', async (req, res) => {
 router.get('/report', async (req, res) => {
   try {
     const { url } = req;
-    console.log(':::::url::::::', url);
+    console.log(':::::get /report::::::', url);
+    const limitArr: RegExpMatchArray | null = url.match(/limit=(\d+(\d)*)/);
+    const pageArr: RegExpMatchArray | null = url.match(/page=(\d+(\d)*)/);
+    const targetArr: RegExpMatchArray | null = url.match(/orderBy=(\w+(\w)*)/);
+    const sortArr: RegExpMatchArray | null = url.match(/orderDir=(\w+(\w)*)/);
+    if (targetArr && sortArr && limitArr && pageArr) {
+      const limit = Number(limitArr[1]);
+      const page = Number(pageArr[1]);
+      const start = limit * page - limit + 1;
+      const end = limit * page;
+      const target = String(targetArr[1]);
+      const sort = String(sortArr[1]);
 
-    const limit = Number(url.slice(14)[0] + url.slice(14)[1]);
-    const page = Number(url.slice(22)[0]);
-    const start = limit * page - limit + 1;
-    const end = limit * page;
-    console.log(':::::limit:::::::', limit);
-    console.log(':::::page:::::::', page);
-    const allReportList = await Report.findAll({
-      raw: true,
-    });
-    const total = String(allReportList.length);
-    const sendReportList = await Report.findAll({
-      raw: true,
-      where: {
-        id: {
-          [Sequelize.Op.between]: [start, end],
+      const allReportList = await Report.findAll({
+        raw: true,
+      });
+      const total = String(allReportList.length);
+      const sendReportList = await Report.findAll({
+        raw: true,
+        where: {
+          id: {
+            [Sequelize.Op.between]: [start, end],
+          },
         },
-      },
-    });
-    if (allReportList) {
-      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
-      res.set('X-Total-Count', total);
-      return res.status(200).json(sendReportList);
+        order: [[target, sort]],
+      });
+      if (allReportList) {
+        res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+        res.set('X-Total-Count', total);
+        return res.status(200).json(sendReportList);
+      }
     }
   } catch (e) {
     return res.sendStatus(500);
